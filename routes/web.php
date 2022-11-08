b6eb4105e54238f719bd8dab733a283226fec5c4
Bikash Dash <radharbikashdash@gmail.com>
2022-11-08 10:51:48 +0530

    creating login routing

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/',function(){
     return view('login');
 });
 
-Route::post('/request', 'LoginController@request');
+// Route::get('/',function(){
+//     return view('login');
+// });
+
+//Route::post('/request', 'LoginController@request');
